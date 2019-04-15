2185e7558c493b6abdf533eb2d47cba005201174
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-04-15 10:41:02 -0700

    cra clean up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  return (
+    <div>
+      APP
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
