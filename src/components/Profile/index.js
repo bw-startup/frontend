de812f66841434b7679a00668e96b2cc36b3ce8c
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-04-18 10:03:04 -0700

    style logout delete buttons in Profile.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,9 +4,6 @@ import * as S from '../../styles';
 export default function Profile(props) {
   return (
     <S.Profile>
-      <button type='button' onClick={props.handleLogOut}>
-        logout
-      </button>
       <S.Title>{props.currentUser.email}</S.Title>
       <S.PredictorInputForm onSubmit={props.handleUpdatePasswordSubmit}>
         <S.PredictorInputStepField>
@@ -33,9 +30,42 @@ export default function Profile(props) {
         {props.updatedMessage && (
           <S.FormMessage success>{props.updatedMessage}</S.FormMessage>
         )}
-        <button type='button' onClick={props.handleDeleteUser}>
-          Delete Account
-        </button>
+
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          <button
+            style={{
+              cursor: 'pointer',
+              borderRadius: '20px',
+              padding: '10px',
+              border: 'none',
+              background: '#ffb3b3',
+              color: 'white'
+            }}
+            type='button'
+            onClick={props.handleDeleteUser}
+          >
+            Delete Account
+          </button>
+          <button
+            style={{
+              cursor: 'pointer',
+              borderRadius: '20px',
+              color: 'white',
+              padding: '10px',
+              border: 'none',
+              background: '#aad4f5'
+            }}
+            type='button'
+            onClick={props.handleLogOut}
+          >
+            Log Out
+          </button>
+        </div>
       </S.PredictorInputForm>
     </S.Profile>
   );
