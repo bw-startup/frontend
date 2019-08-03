836c11589e054b092c7306d4014c10479154395c
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-03 08:42:23 -0700

    share components format

--- src/components/shared/Loader.js
+++ src/components/shared/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
-import { primaryColor } from '../../styles';
+import {primaryColor} from '../../styles';
 import * as S from '../../styles';
 
 export default function(props) {
