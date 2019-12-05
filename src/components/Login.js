0f1b1b925159e9494b29ccc7250abaf5628a907e
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-12-04 16:29:04 -0800

    login onError handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useQuery, useMutation} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
 import * as S from '../styles';
 
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(data: {email: $email, password: $password}) {
+      token
+      user {
+        _id
+        name
+        email
+      }
+    }
+  }
+`;
+
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [loginUser, {loading: loginLoading, error: loginError}] = useMutation(
+    LOGIN_USER,
+    {
+      onCompleted({login}) {
+        console.log('completed');
+      },
+      onError(error) {
+        console.log('error', error.message);
+      },
+    },
+  );
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    console.log('loginloading', loginLoading);
+
+    loginUser({
+      variables: {
+        email: inputs.email,
+        password: inputs.password,
+      },
+    });
+
+    console.log('loginloading', loginLoading);
+    // console.log('loginError', loginError.graphQLErrors);
+
+    setInputs(() => ({
+      email: '',
+      password: '',
+    }));
+
+    console.log('success');
+  };
+
+  const handleInputChange = (event) => {
+    event.persist();
+    setInputs((inputs) => ({
+      [event.target.id]: event.target.value,
+      ...inputs,
+    }));
+  };
+
+  if (loginLoading) return <S.Header>Loading...</S.Header>;
+  if (loginError) return <S.Header>Error</S.Header>;
+
   return (
     <S.AuthenticationContainer>
       <S.AuthenticationSplit>
@@ -17,14 +84,24 @@ const Login = () => {
             Please sign in to view your saved your predictions
           </S.SubHeader>
         </S.AuthenticationHeader>
-        <S.AuthenticationForm>
+        <S.AuthenticationForm onSubmit={handleLogin}>
           <S.AuthenticationField>
             <label htmlFor='email'>Email:</label>
-            <input type='text' name='email' id='email' />
+            <input
+              type='text'
+              name='email'
+              id='email'
+              onChange={handleInputChange}
+            />
           </S.AuthenticationField>
           <S.AuthenticationField>
             <label htmlFor='password'>Password:</label>
-            <input type='password' name='email' id='email' />
+            <input
+              type='password'
+              name='password'
+              id='password'
+              onChange={handleInputChange}
+            />
           </S.AuthenticationField>
           <S.AuthenticationButton type='submit'>Login</S.AuthenticationButton>
         </S.AuthenticationForm>
